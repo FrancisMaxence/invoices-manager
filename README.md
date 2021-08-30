9e112292042f4283a62adfcc82906305e0d71d6c
Francis Maxence <tau.vincentmardon@gmail.com>
2021-08-30 19:14:20 -0400

    i don't remember what i'm doing...

---
+++ README.md
@@ -0,0 +1,6 @@
+# INVOICES MANAGER
+
+A Script to manage invoices file scanned and move to Head Office repository
+
+## Author : Vincent Mardon
+## For "Les Marchés d'aliments naturels et biologique TAU"
--- invoices-manager/index.py
+++ invoices-manager/index.py
@@ -1,19 +1,19 @@
-import shutil, os
+from os import remove
+from shutil import copy, move
 from time import sleep
 
-# Replace Main and Archives by invoices/factures and archives
-
-src = './directory-testing' # Replace by /Desktop (find absolute path) in production mode
+src = './directory-testing/main' # Replace by /Desktop (find absolute path) in production mode
 # Create production and distribution file
 # Find a way to switch between paths easily
 scan_ho = '//PC01-LANGELIER/Scan Ho' # The Head Office repository dist
 test_file = 'hello-world.txt' # The test file
 
-shutil.copy(f'{src}/Main/{test_file}', f'{scan_ho}') # copy original file to Head Office repository
-shutil.move(f'{src}/Main/{test_file}', f'{src}/Main/Archives') # Move original file to ./Archives
+copy(f'{src}/{test_file}', f'{scan_ho}') # copy original file to Head Office repository
+move(f'{src}/{test_file}', f'{src}/archives') # Move original file to ./Archives
 
 
 # Only for testing. Comments those lines when API is ready for production
 sleep(3)
 
-os.remove(f'{scan_ho}/{test_file}')
+move(f'{src}/archives/{test_file}', f'{src}/{test_file}')
+remove(f'{scan_ho}/{test_file}')
