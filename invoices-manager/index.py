aff5b7be3c9bc5013b2094e0a987fe45c9314188
Francis Maxence <tau.vincentmardon@gmail.com>
2021-09-07 19:57:41 -0400

    Add paths file, refactoring index.py

--- invoices-manager/index.py
+++ invoices-manager/index.py
@@ -1,15 +1,15 @@
+# python modules
 from os import remove
 from shutil import copy, move
 from time import sleep
 
-src = './directory-testing/main' # Replace by /Desktop (find absolute path) in production mode
-# Create production and distribution file
-# Find a way to switch between paths easily
-scan_ho = '//PC01-LANGELIER/Scan Ho' # The Head Office repository dist
+# custom modules
+from paths import src, scan_ho
+
 test_file = 'hello-world.txt' # The test file
 
 copy(f'{src}/{test_file}', f'{scan_ho}') # copy original file to Head Office repository
-move(f'{src}/{test_file}', f'{src}/archives') # Move original file to ./Archives
+move(f'{src}/{test_file}', f'{src}/archives') # Move original file to ./archives
 
 
 # Only for testing. Comments those lines when API is ready for production
---
+++ invoices-manager/paths.py
@@ -0,0 +1,4 @@
+src = './directory-testing/main' # Replace by /Desktop (find absolute path) in production mode
+# Create production and distribution file
+# Find a way to switch between paths easily
+scan_ho = '//PC01-LANGELIER/Scan Ho' # The Head Office repository dist
