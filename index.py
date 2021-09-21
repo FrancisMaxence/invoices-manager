9072190448fc346c1e4dcacd8bb5893e1178f802
Francis Maxence <tau.vincentmardon@gmail.com>
2021-09-21 17:51:30 -0400

    improve code, add some suggestions

--- index.py
+++ index.py
@@ -4,6 +4,3 @@ import paths
 
 
 fm = core.File_manager(paths.src, paths.scan_ho)
-
-# For test only, remove when app is ready !
-fm.testing_file_move()
--- paths.py
+++ paths.py
@@ -4,6 +4,7 @@ is_dev = True
 data = './data'
 src = f'{data}/main'
 waiting = f'{data}/waiting'
+archives = f'{data}/archives'
 
 if is_dev:
     scan_ho = f'{data}/scan-ho'
