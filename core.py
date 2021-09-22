4cd9c2106ce89ae508d3d3c7271528a28fec0248
Francis Maxence <tau.vincentmardon@gmail.com>
2021-09-22 16:23:33 -0400

    Improve File_Manager, Add new method. Almost the end !

--- core.py
+++ core.py
@@ -7,47 +7,54 @@ import paths
 import utils
 
 class File_manager:
-    def __init__(self, src, dest, files=False):
-        self.src = src
-        self.dest = dest
+    def __init__(self, main, scan_ho, main_files=False):
+        self.main = main
+        self.scan_ho = scan_ho
         self.waiting = paths.waiting
-        self.files = os.listdir(self.src)
-        self.files_length = len(self.files)
+        
+        # A supprimer lorsque duplicate_file aura été corrigé
+        # Prévoir des test unitaires pour s'assurer du bon fonctionnement de ce code
+        self.main_files = os.listdir(self.main)
+        self.files_length = len(self.main_files)
         
 
     """ 
     Getters
     """
-    def get_src(self):
-        return self.src
+    def get_main(self):
+        return self.main
     
-    def get_dest(self):
-        return self.dest
+    def get_scan_ho(self):
+        return self.scan_ho
     
-    def get_files(self):
-        return self.files
+    def get_main_files(self):
+        return self.main_files
     
     def get_waiting(self):
         return self.waiting
-
-    def get_files_length(self):
-        return self.files_length
     
 
     """
     Methods
     """
-    def copy_file(self):
-        for i in range(self.files_length):
-            shutil.copy(f'{self.src}/{self.files[i-1]}', f'{self.dest}')
-    
-    def move_file(self):
-        for i in range(self.files_length):
-            shutil.move(f'{self.src}/{self.files[i-1]}', f'{self.waiting}')
-    
     def duplicate_file(self):
-        self.copy_file()
-        self.move_file()
+        # Revoir le code, il n'est pas possible de calculer 
+        # la taille du dossier main tant qu'aucune facture n'a été scannée.
+        main_length = len(self.main_files)
+        
+        for i in range(main_length):
+            shutil.copy(f'{self.main}/{self.main_files[i-1]}', f'{self.scan_ho}')
+            shutil.move(f'{self.main}/{self.main_files[i-1]}', f'{self.waiting}')
+    
+    
+    def archive_files(self):
+        waiting_list = os.listdir(self.waiting)
+        scan_ho_list = os.listdir(self.scan_ho)
+        waiting_length = len(waiting_list)
+
+        if waiting_list != scan_ho_list:
+            for i in range(waiting_length):
+                shutil.move(f'{self.waiting}/{waiting_list[i-1]}', f'{paths.archives}')
 
     def testing_file_move(self):
         self.duplicate_file()
@@ -55,8 +62,8 @@ class File_manager:
         time.sleep(3)
 
         for i in range(self.files_length):
-            shutil.move(f'{self.waiting}/{self.files[i-1]}', f'{self.src}')
-            os.remove(f'{self.dest}/{self.files[i-1]}')
+            shutil.move(f'{self.waiting}/{self.main_files[i-1]}', f'{self.main}')
+            os.remove(f'{self.scan_ho}/{self.main_files[i-1]}')
 
 
 
