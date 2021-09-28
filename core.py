e78abe4e0621143fb1138e982474321e4778698f
Francis Maxence <tau.vincentmardon@gmail.com>
2021-09-28 17:27:59 -0400

    Improve FileManager.archive_files method

--- core.py
+++ core.py
@@ -43,10 +43,21 @@ class FileManager:
         waiting_files = os.listdir(self.waiting)
         scan_ho_files = os.listdir(self.scan_ho)
 
-        if waiting_files != scan_ho_files:
+        if len(waiting_files) == 0 and len(scan_ho_files) == 0:
+            #self.testing_file_move()
+            
+            self.duplicate_file()
+        elif len(waiting_files) == 0 and len(scan_ho_files) > 0:
+            #self.testing_file_move()
+            
+            self.duplicate_file()
+        elif waiting_files != scan_ho_files:
             for waiting_file in waiting_files:
                 shutil.move(f'{self.waiting}/{waiting_file}', f'{self.archives}')
-
+            
+            #time.sleep(3)
+            #self.testing_file_move()
+            
             self.duplicate_file()
 
     def testing_file_move(self):
