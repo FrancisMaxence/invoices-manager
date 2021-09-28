41c2197d1f4e09c70502baebd97f43970c765585
Francis Maxence <tau.vincentmardon@gmail.com>
2021-09-28 12:20:42 -0400

    Improve tests

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ fm = core.FileManager()
 data = './data'
 archives = f'{data}/archives'
 main = f'{data}/main'
-dev_scan_ho = f'{data}/scan-ho'
+scan_ho = f'{data}/scan-ho'
 prod_scan_ho = '//PC01-LANGELIER/Scan Ho'
 waiting = f'{data}/waiting'
 
@@ -18,11 +18,7 @@ class TestFileManager(unittest.TestCase):
         self.assertEqual(fm.get_main(), main)
     
     def test_scan_ho_name(self):
-        if paths.is_dev:
-            self.assertEqual(fm.get_scan_ho(), dev_scan_ho)
-        else:
-            self.assertEqual(fm.get_scan_ho(), prod_scan_ho)
-
+        self.assertEqual(fm.get_scan_ho(), scan_ho)
     
     def test_waiting_name(self):
         self.assertEqual(fm.get_waiting(), waiting)
