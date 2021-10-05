663b9d346d5255999f0784b20505bb5012458304
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-05 16:28:37 -0400

    Add on test who work !

--- tests.py
+++ tests.py
@@ -10,6 +10,7 @@ archives = f'{data}/archives'
 main = f'{data}/main'
 scan_ho = f'{data}/scan-ho'
 waiting = f'{data}/waiting'
+files = ['file1.txt','file2.txt', 'file3.txt']
 
 
 class TestFileManager(unittest.TestCase):
@@ -26,7 +27,19 @@ class TestFileManager(unittest.TestCase):
         self.assertEqual(fm.get_waiting(), waiting)
     
     def test_duplicate_file(self):
-        pass
+        for file in files:
+            with open(f'{main}/{file}', 'a'):
+                pass
+                
+        fm.duplicate_file()
+
+        self.assertEqual(os.listdir(scan_ho), files)
+        self.assertEqual(os.listdir(waiting), files)
+
+        for file in files:
+            os.remove(f'{scan_ho}/{file}')
+            os.remove(f'{waiting}/{file}')
+        
 
     def test_archive_files(self):
         pass
