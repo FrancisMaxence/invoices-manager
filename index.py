e00dcef9c6731d530a2a1bb0d8ff0222f418c104
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-11 15:49:56 -0400

    Change some variables

--- index.py
+++ index.py
@@ -5,9 +5,9 @@ from filemanager import FileManager
 
 fm = FileManager()
 
-scan_ho_files = os.listdir(fm.get_scan_ho)
+scan_ho_files = os.listdir(fm.scan_ho)
 scan_ho_len = len(scan_ho_files)
-waiting_files = os.listdir(fm.get_waiting)
+waiting_files = os.listdir(fm.waiting)
 waiting_len = len(waiting_files)
 
 if waiting_len == 0 and scan_ho_len == 0:
