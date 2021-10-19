6b41046f991e8adfd035931b495a55dd5dc19181
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-19 12:40:34 -0400

    add new file

--- index.py
+++ index.py
@@ -1,19 +1,3 @@
-import os
+from core.comparedir import compare_directories
 
-# modules
-from core.filemanager import FileManager
-
-fm = FileManager()
-
-scan_ho_files = os.listdir(fm.scan_ho)
-scan_ho_len = len(scan_ho_files)
-waiting_files = os.listdir(fm.waiting)
-waiting_len = len(waiting_files)
-
-if waiting_len == 0 and scan_ho_len == 0:
-    fm.duplicate_file()
-elif waiting_len == 0 and scan_ho_len > 0:
-    fm.duplicate_file()
-elif waiting_files != scan_ho_files:
-    fm.archive_files()
-    fm.duplicate_file()
+compare_directories()
