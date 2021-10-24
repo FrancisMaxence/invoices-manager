d50973b6dca99fa93ed3ac4f13a76bf7dd112fb7
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-24 16:27:48 -0400

    Change comments

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from core.comparedir import compare_folders
 
+# Once at launch
+#   if invoice date > 6 month:
+#       remove invoice (regex or if [date] > 6 month)
+
 # Loop
 # each 30 minutes
 compare_folders()
-
-#   if invoice date > 6 month:
-#       remove invoice (regex or if [date] > 6 month)
 # until computer shutdown
