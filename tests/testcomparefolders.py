ac666dd424c89e0f10f2a153ee29f5adfe7cbd1a
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-25 18:36:28 -0400

    Add 1/4 test

--- tests/testcomparefolders.py
+++ tests/testcomparefolders.py
@@ -1,12 +1,15 @@
 import unittest
+import os
 
 import testsetup as ts
 from core.comparefolders import compare_folders
 
-class TestCompareDirectories(unittest.TestCase):
+class TestCompareFolders(unittest.TestCase):
     def test_waiting_and_scan_ho_empty(self):
-        # Nothing in the two folders
-        pass
+        compare_folders()
+        self.assertEqual(len(os.listdir(ts.main)), 0)
+        self.assertListEqual(os.listdir(ts.waiting), os.listdir(ts.scan_ho))
+        self.assertEqual(len(os.listdir(ts.archives)), 0)
 
     def test_waiting_greater_than_scan_ho(self):
         # Waiting folder have still files, Scan Ho folder is empty
@@ -19,3 +22,6 @@ class TestCompareDirectories(unittest.TestCase):
     def test_waiting_less_than_scan_ho(self):
         # Waiting directory is empty but not Scan Ho directory
         pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
