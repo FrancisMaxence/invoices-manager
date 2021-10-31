215a5c35f550d0e397f0be8d0b959ef635a129e5
Francis Maxence <tau.vincentmardon@gmail.com>
2021-10-31 11:52:38 -0400

    Add the last loop (YEAAAAH), and some comments

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
+import time
+import datetime
+
 from core.comparefolders import compare_folders
 
 # Once at launch
 #   if invoice date > 6 month:
 #       remove invoice (regex or if [date] > 6 month)
+app = True
 
-# Loop
-# each 30 minutes
-compare_folders()
-# until computer shutdown
+while app:
+    print(f'Checkout: {datetime.datetime.now()}')
+    compare_folders()
+    time.sleep(1800)
--- tests/testsetup.py
+++ tests/testsetup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Allow user to launch tests in root or tests directory
+# Allow user to launch tests in root or tests folders
 if 'tests' not in os.getcwd():
     sys.path.insert(1, os.getcwd())
     data = './data'
@@ -13,6 +13,8 @@ archives = f'{data}/archives'
 main = f'{data}/main'
 scan_ho = f'{data}/scan-ho'
 waiting = f'{data}/waiting'
+
+# Change files name for cleanup module (*_[date-us].pdf)
 files = ['file1.txt','file2.txt', 'file3.txt']
 second_scan = ['file4.txt'] # For TestApp only
 
