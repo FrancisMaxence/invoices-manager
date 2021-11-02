ec4dc7068dedc0094183d64c0f32c208f9cbbe7d
Francis Maxence <tau.vincentmardon@gmail.com>
2021-11-02 16:39:51 -0400

    Add logs

--- core/comparefolders.py
+++ core/comparefolders.py
@@ -4,6 +4,7 @@ from core.filemanager import FileManager
 def compare_folders():
     fm = FileManager()
 
+    main_files = os.listdir(fm.main)
     scan_ho_files = os.listdir(fm.scan_ho)
     scan_ho_len = len(scan_ho_files)
     waiting_files = os.listdir(fm.waiting)
@@ -14,3 +15,6 @@ def compare_folders():
     elif waiting_len > 0 and scan_ho_len == 0:
         fm.archive_files()
         fm.duplicate_file()
+    
+    # For utils.log()
+    return main_files, waiting_files
---
+++ utils/logs.py
@@ -0,0 +1,10 @@
+import utils.paths
+
+def logs(main, waiting):
+
+    # A refaire pour prendre en compte les dossiers vides
+    if main:
+        print()
+    if waiting:
+        print()
+    
