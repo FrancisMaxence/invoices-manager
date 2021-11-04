32dad893d7bf326651263fb7b6d9ed49a2e2e018
Francis Maxence <tau.vincentmardon@gmail.com>
2021-11-04 11:23:06 -0400

    Change colors

--- core/comparefolders.py
+++ core/comparefolders.py
@@ -10,8 +10,8 @@ def compare_folders():
     waiting_files = os.listdir(fm.waiting)
     waiting_len = len(waiting_files)
 
-    log_main = f'\033[1;32;40m main/ -> waiting/ and scan-ho/:\033[1;37;40m {", ".join(main_files)}'
-    log_waiting = f'\033[1;32;40m waiting/ -> archives/: \033[1;37;40m {", ".join(waiting_files)}'
+    log_main = f'\033[1;32;40m main/ -> waiting/ and scan-ho/\033[1;37;40m: {", ".join(main_files)}'
+    log_waiting = f'\033[1;32;40m waiting/ -> archives/\033[1;37;40m: {", ".join(waiting_files)}'
     log_nothing = '\033[1;33;40m Nothing changed \033[1;37;40m'
 
     if waiting_len == 0 and scan_ho_len == 0:
