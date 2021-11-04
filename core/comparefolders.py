d0023c8c0f296bfca26e54d1d4c8e987e396bf0c
Francis Maxence <tau.vincentmardon@gmail.com>
2021-11-04 09:18:00 -0400

    Fix bugs

--- core/comparefolders.py
+++ core/comparefolders.py
@@ -12,16 +12,22 @@ def compare_folders():
 
     log_main = f'\033[1;32;40m main/ -> waiting/ and scan-ho/:\033[1;37;40m {", ".join(main_files)}'
     log_waiting = f'\033[1;32;40m waiting/ -> archives/: \033[1;37;40m {", ".join(waiting_files)}'
+    log_nothing = '\033[1;33;40m Nothing changed \033[1;37;40m'
 
     if waiting_len == 0 and scan_ho_len == 0:
         fm.duplicate_file()
-        print(log_main)
+        if len(main_files) > 0:
+            print(log_main)
+        else:
+            print(log_nothing)
+        return 0
     elif waiting_len > 0 and scan_ho_len == 0:
         fm.archive_files()
         fm.duplicate_file()
         if main_files:
             print(log_main)
         print(log_waiting)
+        return 0
     else:
-        print('\033[1;33;40m Nothing changed \033[1;37;40m')
+        print(log_nothing)
         return 1
