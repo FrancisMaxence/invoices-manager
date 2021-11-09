4ec36d2b69fcd27e568a4bcf925d64d16fd76cc8
Francis Maxence <tau.vincentmardon@gmail.com>
2021-11-09 16:21:49 -0500

    Prepare testsetup and invoices_cleaner for future tests

--- tests/testsetup.py
+++ tests/testsetup.py
@@ -14,9 +14,8 @@ main = f'{data}/main'
 scan_ho = f'{data}/scan-ho'
 waiting = f'{data}/waiting'
 
-# Change files name for cleanup module (*_[date-us].pdf)
-files = ['file1.txt','file2.txt', 'file3.txt']
-second_scan = ['file4.txt'] # For TestApp only
+files = ['file1_2020-12-25_102033568.txt','file2_2021-11-09_161545123.txt', 'file3_2021-08-12_085942816.txt']
+second_scan = ['file4_2021-03-29_045113946.txt'] # For TestApp only
 
 
 def create_files(path):
