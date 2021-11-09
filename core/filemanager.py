c35ae0322609749da70ee65de1f146da5c20b852
Francis Maxence <tau.vincentmardon@gmail.com>
2021-11-09 13:19:41 -0500

    Add new method sketch on FileManager

--- core/filemanager.py
+++ core/filemanager.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import datetime
 
 import utils.paths as paths
 
@@ -26,3 +27,13 @@ class FileManager:
 
         for waiting_file in waiting_files:
             shutil.move(f'{self.waiting}/{waiting_file}', f'{self.archives}')
+    
+    def invoices_cleaner(self):
+        archives_files = os.listdir(self.archives)
+        today = datetime.datetime.now() # <class 'datetime.datetime(YYYY, MMMM, DD, HH, MM, SS, mmmmmm)'>
+        # 6 months = 183 days
+
+        for file in archives_files:
+            file_date = file.split('_')
+            # join file_date[1] with file_date[2] -> strptime("%Y%m%d%H%"m"%s%ms") (Find code for minutes and milliseconds)
+            # file_date - today >= 183 day => remove file
